71af132725897a73dc487d175d00f02652682f0f
Govinda Fichtner <govinda.fichtner@googlemail.com>
2018-10-24 11:31:49 +0200

    add inital unit for lib

--- tests/unit/test_users_get.py
+++ tests/unit/test_users_get.py
@@ -1,2 +1,10 @@
-def test_hello():
-    pass
+from hamcrest import assert_that, equal_to
+from lib.users_get import handler
+
+def test_users_get():
+    event = {}
+    context = {}
+
+    response = handler(event, context)
+
+    assert_that(response['statusCode'], equal_to(200))
